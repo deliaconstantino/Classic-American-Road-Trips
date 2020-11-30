7160fd9069c197becec6a47ff697e89602e0e351
deliaconstantino <dconstan92@gmail.com>
2020-11-30 16:48:56 -0500

    scraping

--- lib/classic_american_road_trips/route.rb
+++ lib/classic_american_road_trips/route.rb
@@ -1,8 +1,19 @@
 class ClassicAmericanRoadTrips::Route
   attr_accessor :name, :distance, :start_point, :end_point, :url, :related_guidebook, :description
 
-  def create_from_index_page
+  def new_from_index_page(r)
     #is called from an iterative method scraper class and assigns the basic info to each route:
+    r = self.new
+    @name = r.css("h2").text
+    r.css("p").each.with_index do | p, i|
+      case i
+        when 1
+          @paragraph = p.text
+        when 2
+          @highlights = p.text
+        end
+      @guidebook = p.css("em").text
+    end
     #name
     #paragraph
     #url??
--- lib/classic_american_road_trips/scraper.rb
+++ lib/classic_american_road_trips/scraper.rb
@@ -6,35 +6,77 @@ class ClassicAmericanRoadTrips::Scraper
      # page.css(".col-md-4").css("h2").text
 
 #this returns route names, except also has the empty ones
-    page.css(".col-md-4").each do |r|
-      puts r.css("h2").text
 
-      end
-
-        page.css(".col-md-4").each do |r|
-         puts r.css("p").text
-       end
-
-       page.css(".col-md-4").each do |r|
-         r.css("p").each.with_index do | p, i|
-           puts "#{i}. #{p}"
-        end
-      end
-
-
-      p_array = []
+      # route = ClassicAmericanRoadTrips::Route.new(r)
+      # route.create_from_index_page
       page.css(".col-md-4").each do |r|
-        r.css("p").each.with_index do | p, i|
-          puts "#{i}. #{p}"
-          if i == 1
-            p_array << p.text
+      ClassicAmericanRoadTrips::Route.new_from_index_page(r)
+      # puts r.css("h2").text
+
+      end
+
+
+      grabbed from route page:
+      r.css("h2").text  #name
+      r.css("p").each.with_index do | p, i|
+        case i
+          when 1
+            @paragraph = p.text
+            "paragraph: #{p.text}"
+          when 2
+            @highlights = p.text
+            "highlights: #{p.text}"
           end
+      end
+
+
+      page.css(".col-md-4").each do |r|
+       r.css("p").each.with_index do | p, i|
+         puts "NEW ROUTE!!!!"
+         case i
+         when 1
+           puts "paragraph: #{p.text}"
+         when 2
+           puts "highlights: #{p.text}"
+         end
+         puts p.css("em").text
        end
      end
+
+
+
+
+
+
+
+
+
+
+
+
+     #    page.css(".col-md-4").each do |r|
+     #     puts r.css("p").text
+     #   end
+     #
+     #   page.css(".col-md-4").each do |r|
+     #     r.css("p").each.with_index do | p, i|
+     #       puts "#{i}. #{p}"
+     #    end
+     #  end
+     #
+     #
+     #  p_array = []
+     #  page.css(".col-md-4").each do |r|
+     #    r.css("p").each.with_index do | p, i|
+     #      puts "#{i}. #{p}"
+     #      if i == 1
+     #        p_array << p.text
+     #      end
+     #   end
+     # end
             # if i == 1
             #   puts p.css("p").text
             # end
     end
     # puts routes
   end
-end
